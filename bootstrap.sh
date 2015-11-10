2fc91ba2f867420e1b9c913d1ce1e5d92cc1c25c
VireshDoshi <vireshdoshi@time2test.co.uk>
2015-11-10 10:13:09 +0000

    changes...

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,8 @@
 
 # Install docker
 apt-get update -qq
-#apt-get install -y docker.io
-#ln -sf /usr/bin/docker.io /usr/local/bin/docker
+apt-get install -y docker.io
+ln -sf /usr/bin/docker.io /usr/local/bin/docker
 #sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
 
 # Install tcl
